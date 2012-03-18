0d991131d23bba3baeae31aeaa33530901676f62
Thav <tony@crete.(none)>
2012-03-18 12:13:40 -0400

    Local configuration.h

--- Sprinter/Configuration.h
+++ Sprinter/Configuration.h
@@ -9,12 +9,12 @@
 // Gen6 = 5, 
 // Sanguinololu up to 1.1 = 6
 // Sanguinololu 1.2 and above = 62
-// Gen 7 @ 16MHZ only= 7
 // Teensylu (at90usb) = 8
 // Gen 3 Plus = 21
 // gen 3  Monolithic Electronics = 22
 // Gen3 PLUS for TechZone Gen3 Remix Motherboard = 23
 #define MOTHERBOARD 5 
+#define GEN6HBPMOD 1
 
 //// Thermistor settings:
 // 1 is 100k thermistor
@@ -24,15 +24,12 @@
 // 5 is ParCan supplied 104GT-2 100K
 // 6 is EPCOS 100k
 // 7 is 100k Honeywell thermistor 135-104LAG-J01
-#define THERMISTORHEATER 1
-#define THERMISTORBED 1
-//// Gen 6 Heated Build Plate
-// Set to 1 if using the Gen 6 HBP mod here <wikilink>
-#define GEN6HBPMOD 1
+#define THERMISTORHEATER 6
+#define THERMISTORBED 6
 
 //// Calibration variables
 // X, Y, Z, E steps per unit - Metric Prusa Mendel with Wade extruder:
-float axis_steps_per_unit[] = {80, 80, 3200/1.25,700}; 
+float axis_steps_per_unit[] = {40.211869, 40.05005, 3373.854,270.470445};
 // Metric Prusa Mendel with Makergear geared stepper extruder:
 //float axis_steps_per_unit[] = {80,80,3200/1.25,1380}; 
 // MakerGear Hybrid Prusa Mendel:
@@ -44,24 +41,20 @@ float axis_steps_per_unit[] = {80, 80, 3200/1.25,700};
 // The pullups are needed if you directly connect a mechanical endswitch between the signal and ground pins.
 //If your axes are only moving in one direction, make sure the endstops are connected properly.
 //If your axes move in one direction ONLY when the endstops are triggered, set [XYZ]_ENDSTOP_INVERT to true here:
-const bool X_ENDSTOP_INVERT = false;
-const bool Y_ENDSTOP_INVERT = false;
-const bool Z_ENDSTOP_INVERT = false;
+const bool X_ENDSTOP_INVERT = true;
+const bool Y_ENDSTOP_INVERT = true;
+const bool Z_ENDSTOP_INVERT = true;
 
 // This determines the communication speed of the printer
 #define BAUDRATE 115200
 
 // Comment out (using // at the start of the line) to disable SD support:
-#define SDSUPPORT
+//#define SDSUPPORT
 // Uncomment to make Sprinter run init.g from SD on boot
 //#define SDINITFILE
 
-//// ADVANCED SETTINGS - to tweak parameters
 
-#ifdef SDSUPPORT
-	//Fast transfer chunk size (> 1024 is unstable, change at your own risk).
-	#define SD_FAST_XFER_CHUNK_SIZE 1024
-#endif
+//// ADVANCED SETTINGS - to tweak parameters
 
 #include "thermistortables.h"
 
@@ -78,7 +71,7 @@ const bool DISABLE_Z = true;
 const bool DISABLE_E = false;
 
 // Inverting axis direction
-const bool INVERT_X_DIR = false;
+const bool INVERT_X_DIR = true;
 const bool INVERT_Y_DIR = false;
 const bool INVERT_Z_DIR = true;
 const bool INVERT_E_DIR = false;
@@ -91,13 +84,13 @@ const bool INVERT_E_DIR = false;
 
 const bool min_software_endstops = false; //If true, axis won't move to coordinates less than zero.
 const bool max_software_endstops = true;  //If true, axis won't move to coordinates greater than the defined lengths below.
-const int X_MAX_LENGTH = 200;
-const int Y_MAX_LENGTH = 200;
-const int Z_MAX_LENGTH = 100;
+const int X_MAX_LENGTH = 180;
+const int Y_MAX_LENGTH = 160;
+const int Z_MAX_LENGTH = 80;
 
 //// MOVEMENT SETTINGS
 const int NUM_AXIS = 4; // The axis order in all axis related arrays is X, Y, Z, E
-float max_feedrate[] = {200000, 200000, 240, 500000};
+float max_feedrate[] = {200000, 200000, 130, 500000};
 float homing_feedrate[] = {1500,1500,120};
 bool axis_relative_modes[] = {false, false, false, false};
 
@@ -108,12 +101,6 @@ bool axis_relative_modes[] = {false, false, false, false};
 // Step delay over interval ratio. If you are still experiencing missing steps, try to uncomment the following line, but be aware this
 // If you enable this, make sure STEP_DELAY_MICROS is disabled. (except for Gen6: both need to be enabled.)
 //#define STEP_DELAY_RATIO 0.25
-
-///Oscillation reduction.  Forces x,y,or z axis to be stationary for ## ms before allowing axis to switch direcitons.  Alternative method to prevent skipping steps.  Uncomment the line below to activate.
-//#define RAPID_OSCILLATION_REDUCTION
-#ifdef RAPID_OSCILLATION_REDUCTION
-long min_time_before_dir_change = 30; //milliseconds
-#endif
 
 // Comment this to disable ramp acceleration
 #define RAMP_ACCELERATION
@@ -179,7 +166,7 @@ char uuid[] = "00000000-0000-0000-0000-000000000000";
 //#define TEMP_HYSTERESIS 5       // (C°) range of +/- temperatures considered "close" to the target one
 
 //// The minimal temperature defines the temperature below which the heater will not be enabled
-#define MINTEMP 5
+#define MINTEMP 0
 
 //// Experimental max temp
 // When temperature exceeds max temp, your heater will be switched off.
